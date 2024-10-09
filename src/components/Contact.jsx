e8528bd5f3e9b7b278d931a11f6a5d3a2e9e469d
Ryan <kuglerryan@gmail.com>
2024-10-09 13:32:15 -0400

    contact form UI + asset path changes in portfolio.js

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,12 @@ function Contact() {
                 <form action="https://getform.io/slug"
                       method="POST"
                       className="flex flex-col w-full md:w-7/12">
-                
-                    <input type="text" name="name" placeholder="Name" 
-                           className="p-2 bg-transparent border-2 rounded-md focus:outline-none"/>
+                    <input type="text" name="name" placeholder="Name" className="p-2 bg-transparent border-2 rounded-md focus:outline-none"/>
+                    <input type="text" name="email" placeholder="Email" className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"/>
+                    <textarea name="message" placeholder="Message" rows="10" className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"></textarea>
+                    <button type="button" className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-pink-500 to-yellow-500 drop-shadow-md hover:stroke-white">
+                        Connect!
+                    </button>
                 </form>
             </div>
         </div>
