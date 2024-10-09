ee4b822f76edd2c050ea66aaabf07e05269431dc
Ryan <kuglerryan@gmail.com>
2024-10-09 16:40:09 -0400

    Change button type from 'button' to 'submit' in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,7 +13,7 @@ function Contact() {
                     <input type="text" name="name" placeholder="Name" className="p-2 bg-transparent border-2 rounded-md focus:outline-none"/>
                     <input type="text" name="email" placeholder="Email" className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"/>
                     <textarea name="message" placeholder="Message" rows="10" className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"></textarea>
-                    <button type="button" className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-pink-500 to-yellow-500 drop-shadow-md hover:stroke-white">
+                    <button type="submit" className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-pink-500 to-yellow-500 drop-shadow-md hover:stroke-white">
                         Connect!
                     </button>
                 </form>
